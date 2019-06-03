881ff8f0725984dd0074f36753d5d90b1db0334f
Michael Jones <michael.jones90@me.com>
2019-06-03 15:18:05 +0100

    Initial Commit

---
+++ TH1_SoccerLeagueCoordinator/TH1_SoccerLeagueCoordinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TH1_SoccerLeagueCoordinator
+//
+//  Created by Michael Jones on 03/06/2019.
+//  Copyright © 2019 Michael Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TH1_SoccerLeagueCoordinator/TH1_SoccerLeagueCoordinatorTests/TH1_SoccerLeagueCoordinatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  TH1_SoccerLeagueCoordinatorTests.swift
+//  TH1_SoccerLeagueCoordinatorTests
+//
+//  Created by Michael Jones on 03/06/2019.
+//  Copyright © 2019 Michael Jones. All rights reserved.
+//
+
+import XCTest
+@testable import TH1_SoccerLeagueCoordinator
+
+class TH1_SoccerLeagueCoordinatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
